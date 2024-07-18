6da465394a9a9b2d9da3c9e480d087c36dd8c821
ASarriJackson <asarri.jackson@gmail.com>
2024-07-18 16:17:17 +0100

    Added different splits

--- Split_functions_classify/splits_ten.py
+++ Split_functions_classify/splits_ten.py
@@ -3,7 +3,7 @@ from helper_fun import *
 #read_csv file titled compounds_filtered.csv from directory COVID_MOONSHOT
 
 def ten_split(filt_comp, target = 'f_avg_pIC50'): #arg1: name of table, arg2: target (default = 'f_avg_pIC50')
-    filt_comp['10_bin'] = pd.qcut(filt_comp[target], 13, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],  duplicates='drop', precision=100)
+    filt_comp['bin_10'] = pd.qcut(filt_comp[target], 13, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],  duplicates='drop', precision=100)
     return filt_comp
 
 #plot a histogram of the distribution of the bin values afterpassing filt_comp through three_split
--- Split_functions_classify/splits_three.py
+++ Split_functions_classify/splits_three.py
@@ -3,7 +3,7 @@ from helper_fun import *
 #read_csv file titled compounds_filtered.csv from directory COVID_MOONSHOT
 
 def three_split(filt_comp, target = 'f_avg_pIC50'): #arg1: name of table, arg2: target (default = 'f_avg_pIC50')
-    filt_comp['3_bin'] = pd.qcut(filt_comp[target], 3, labels = ['low', 'medium', 'high'])
+    filt_comp['bin_3'] = pd.qcut(filt_comp[target], 3, labels = ['low', 'medium', 'high'])
     return filt_comp
 
 #plot a histogram of the distribution of the bin values afterpassing filt_comp through three_split
